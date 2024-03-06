17698f768bbdb5194e0bbdc79e4f5d4fed7c5749
Pavlo Valerko <s30424@pjwstk.edu.pl>
2024-03-06 13:32:59 +0100

    Excercise 1

---
+++ untitled/untitled/src/Main.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        Scanner Scanner = new Scanner(System.in);
+        int number;
+        System.out.println("Enter your number:");
+        number= Scanner.nextInt();
+        if(number==0){
+            System.out.println("Value is 0");}
+            else if (number>0){
+                System.out.println("the nymber is positive");}
+            else {
+                    System.out.println("The nymber is neg");
+            }
+
+        }
+
+        }
+
